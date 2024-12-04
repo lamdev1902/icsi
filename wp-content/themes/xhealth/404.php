05a39a5185e79c01ada70da2fcbf653774e4d563
truclam1902 <nguyenhongtruclam1@gmail.com>
2024-12-04 15:24:39 +0700

    theme update

--- wp-content/themes/xhealth/404.php
+++ wp-content/themes/xhealth/404.php
@@ -1,13 +1,14 @@
 <?php get_header(); ?>
 <main class="error-container">
-<div class="error-content">
-	<div class="error-img">
-		<img class="img-left" src="https://www.icsi.org/wp-content/uploads/2024/12/404-icsi-left.svg" alt="">
-		<img class="img-right" src="https://www.icsi.org/wp-content/uploads/2024/12/404-icsi-right.svg" alt="">	
+	<div class="container">
+		<div class="error-content">
+			<h1>Oops! <span style="display: block;">That page can't be found.</span> </h1>
+			<p>The page requested couldn't be found. This could be a spelling error in the URL or a removed page</span>
+			</p>
+			<div class="error-img">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/error.svg" alt="" />
+			</div>
+		</div>
 	</div>
-	<h1>Oops! <span>That page can’t be found.</span> </h1>
-	<p>The page requested couldn’t be found.<span>This could be a spelling error in the URL or a revoved page</span></p>
-	<a href="<?php echo home_url(); ?>" class="button-blue-shadow-black error-button">SEE ALL POST</a>
-</div>
 </main>
 <?php get_footer(); ?>
